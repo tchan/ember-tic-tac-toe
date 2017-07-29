e66257b9c8c198a1b616a9aac2212acc4b6c6caf
Travis Chan <travismchan@gmail.com>
2017-07-29 18:41:37 +1000

    added modal and selecting symbol

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,4 +1,25 @@
 import Ember from 'ember';
+const { get, set } = Ember;
 
 export default Ember.Controller.extend({
+  showDialog: true,
+  playerOne: null,
+  computer: null,
+
+  actions: {
+    closeDialog() {
+      set(this, 'showDialog', false);
+    },
+    selectSymbol(symbol) {
+      // console.log('symbol selected: ', symbol);
+      set(this, 'playerOne', symbol);
+      if (symbol === 'X') {
+        set(this, 'computer', 'O');
+      }
+      else {
+        set(this, 'computer', 'X');
+      }
+      set(this, 'showDialog', false);
+    }
+  }
 });
